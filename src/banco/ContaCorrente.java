777b1d32afb68212b10ed01ccddb84ee910ed714
Fernando Fernandes <106794665+fernandofernandess@users.noreply.github.com>
2022-06-30 22:17:58 -0300

    Initial commit

---
+++ src/banco/ContaCorrente.java
@@ -0,0 +1,9 @@
+package banco;
+
+public class ContaCorrente extends Conta{
+    @Override
+    public void imprimirExtrato() {
+        System.out.println("### Extrato Conta Corrente ###");
+        imprimirInfosComuns();
+    }
+}
---
+++ src/banco/ContaPoupanca.java
@@ -0,0 +1,11 @@
+package banco;
+
+public class ContaPoupanca extends Conta{
+    @Override
+    public void imprimirExtrato() {
+        System.out.println("### Extrato Conta Poupança ###");
+        imprimirInfosComuns();
+    }
+
+
+}
